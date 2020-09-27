884a21f0c47da1a662655749a7437d0667044275
Daniela Bresler <dbresler@github.com>
2020-09-27 15:54:59 -0500

    updated code

---
+++ forms/strings/strings.js
@@ -0,0 +1,21 @@
+let quoteString = ("Failure is simply the opportunity to begin again this time more intelligently.")
+
+let upperCaseString = quoteString.toUpperCase()
+alert(`Upper Case String is ${upperCaseString}`)
+
+let authorString = ("- Henry Ford")
+alert(authorString)
+
+let completeString = quoteString + authorString
+alert(completeString)
+
+let found = ("Henry is in the quoteString")
+console.log(found)
+
+let secondQuote = ("Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King")
+let lowerCaseString = secondQuote.toLowerCase()
+console.log(lowerCaseString)
+
+let userName = prompt(Enter a number) 
+let y = 10 
+let answer = Caculate(userName,y)
